a1f4b8fbd19133189b3d3aae8356b5fc612d6499
Gerard01mm <56315258+Gerard01mm@users.noreply.github.com>
2023-11-21 21:34:28 +0100

    Modificación error LoginPage.vue

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -72,7 +72,7 @@ export default {
             }
 
             try {
-                const url = "https://backendrecipes.onrender.com/register/"
+                const url = "https://backendrecipes.onrender.com/login/"
                 let response = await axios.post(url, {
                     username: this.username,
                     email: this.email,
